79166424d6433a88e95a401bd5f6e1e35013867e
DasuSiddharth <86275058+DasuSiddharth@users.noreply.github.com>
2021-09-10 12:42:04 +0530

    Create InteractiveWebPage.js

---
+++ InteractiveWebPage.js
@@ -0,0 +1,16 @@
+function changecolor()
+{
+  var dd1=document.getElementById("div1");
+  var dd2=document.getElementById("div2");
+  
+  dd1.className="gay1";  
+  dd2.className="gay2";
+}
+function changetext()
+{
+  var dd1=document.getElementById("div1");
+  var dd2=document.getElementById("div2");
+  
+  dd1.innerHTML="chal na chutiye";  
+  dd2.innerHTML="gandmara";
+}
